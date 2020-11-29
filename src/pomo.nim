91f75604174bfcf57093481a9ab22c085bdf64de
divanov <dmitry.ivanov.ev@gmail.com>
2020-11-29 20:30:27 +0100

    removing illwill; back to the simplets basics

--- src/pomo.nim
+++ src/pomo.nim
@@ -1,6 +1,7 @@
 import strutils, chronos, threadpool
+import times, std/monotimes, strformat, os
 
-type EventType {. pure .} = enum Time, Interruption
+type PeriodType {. pure .} = enum WorkPer, PausePer
 
 const minute = 1000
 const pause = 5 * minute
@@ -9,21 +10,35 @@ const work = 15 * minute
 const workMessage: string = "25 minutes of concentrated work\a\a\a\a\a"
 const pauseMessage: string = "5 minutes of recreation\a\a\a"
 
-proc iterationLogic(): Future[void] {. async .} =
-  var pomodoros = 0
-  while true:
-    echo workMessage, (work/1000)
-    yield sleepAsync(work.milliseconds)
-    echo pauseMessage, (pause/1000)
-    yield sleepAsync(pause.milliseconds)
+proc baseTime(pert : PeriodType): int =
+  case pert
+  of WorkPer:
+    return 25 * minute
+  of PausePer:
+    return 5 * minute
 
-proc sidecarThread() : void {. thread .} =
-  waitFor iterationLogic()
-
-proc kinter(): Future[void] {. async .} =
-  echo stdin.readLine()
+proc flip(pt: PeriodType): PeriodType =
+  case pt
+  of WorkPer :
+    return PausePer
+  of PausePer:
+    return WorkPer
 
 proc main(): void =
-  waitFor(wait(kinter(), 3000.milliseconds))
+  var pomodoros = 0
+  var period = WorkPer
+  let startPer = getMonoTime()
+  var finishPer = startPer + initDuration(milliseconds = baseTime(period))
+  while true:
+    stdout.write(fmt "\r{pomodoros} pomos, {period}")
+    
+    sleep(1000)
+
+    let mt = getMonoTime()
+    if finishPer < mt:
+      if period == WorkPer:
+        inc(pomodoros)
+      period = flip(period)
+      finishPer = mt + initDuration(milliseconds = baseTime(period))
 
 main()
